3a4c33181628df9d0269b33706d8e1d06b30b0ff
Indranil Paul <indranil.paul04@gmail.com>
2024-09-17 12:22:26 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="bg-[#FFD7C4] flex justify-between items-center h-10 ">
+    <nav className="bg-[#FFD7C4] flex justify-between items-center h-10 w-full">
         <div className="logo px-5 font-bold text-blue-600 flex items-center">
         <iframe className='w-16' src="https://lottie.host/embed/8575a855-a015-4f63-932a-55131396798c/Ez9U48NjoS.json"></iframe>
           <a className='text-xl' href="#">iPassword</a>
